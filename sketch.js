0eccf5505a909f252d671b77f0aa94ee6863eae1
Ritvik509 <78469878+Ritvik509@users.noreply.github.com>
2021-08-21 12:46:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var database;
+var canvas,backgroundImage;
+var playerCount;
+var gameState = 0;
+var form,player,game;
+function setup(){
+    database=firebase.database()
+    canvas=createCanvas(400,400);
+    game = new Game()
+    game.getState()
+    game.start()
+}
+
+
+function draw(){
+   
+}
+
